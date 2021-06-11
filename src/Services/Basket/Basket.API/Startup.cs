57571640b0d51054d23ab552d28407143c176c18
Uche Igbokwe <uchehenryigbokwe@gmail.com>
2021-06-11 08:29:43 +0100

    setup rabbitmq and mass transit

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,11 +30,21 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Redis Configuration
             services.AddStackExchangeRedisCache(options => options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString"));
+
+            //General config
             services.AddScoped<IBasketRepository, BasketRepository>();
+
+            //GRPC config
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
                 (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
             services.AddScoped<DiscountGrpcService>();
+
+            //Mass transit-RabbitMq Config
+            services.AddMassTransit(config => config.UsingRabbitMq((ctx, cfg) => cfg.Host(Configuration["EventBusSettings:HostAddress"])));
+            services.AddMassTransitHostedService();
+            
             services.AddControllers();
             services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" }));
         }
