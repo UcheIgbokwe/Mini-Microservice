d94f5581ad814b54b65f817107148babf8eeea76
Uche Igbokwe <uchehenryigbokwe@gmail.com>
2021-06-30 00:34:35 +0100

    final commit

---
+++ src/Client/AspnetRunBasics/Models/BasketItemExtendedModel.cs
@@ -0,0 +1,12 @@
+namespace AspnetRunBasics.Models
+{
+    public class BasketItemExtendedModel
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+
+    }
+}
